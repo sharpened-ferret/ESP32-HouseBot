e3340efd6c7c4f19b6ae1d129f0c0c728e2dbd9f
Neil Woodhouse <clonesupreme@gmail.com>
2022-09-18 15:04:53 +0100

    core discord connection working

--- main/main.c
+++ main/main.c
@@ -24,9 +24,10 @@
 #define MAXIMUM_RETRY CONFIG_MAXIMUM_RETRY
 
 // Discord connection settings
+#define CHANNEL_ID CONFIG_CHANNEL_ID
 #define CONNECTION_MSG_ENABLED CONFIG_CONNECTION_MESSAGE_ENABLED
 #define CONNECTION_MSG CONFIG_CONNECTION_MESSAGE
-#define CHANNEL_ID CONFIG_CHANNEL_ID
+
 
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
@@ -39,6 +40,28 @@ static discord_handle_t bot;
 static const char *TAG = "wifi station";
 
 static int s_retry_num = 0;
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
 
 // WiFi Initialisation
 void wifi_init_sta(void)
